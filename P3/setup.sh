123df232ac09258b7cf1f4344c43c731b8d77e67
afaraji <alegi.bruno1@gmail.com>
2022-01-11 05:38:37 +0100

    part 3 almost done

--- P3/setup.sh
+++ P3/setup.sh
@@ -8,9 +8,9 @@ RED='\033[1;31m' # Red Color
 GREEN='\033[1;32m' # Green Color
 NC='\033[0m' # No Color
 sudo apt update
-sudo apt install -y net-tools
-sudo apt install -y curl
-sudo apt install -y vim
+sudo apt install -y -qq net-tools
+sudo apt install -y -qq curl
+sudo apt install -y -qq vim
 
 
 # install docker
@@ -20,13 +20,16 @@ sudo curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo systemctl start docker
 sudo systemctl enable docker
-docker ps && echo -e "${GREEN}[INFO] Docker installed successful${NC}" || echo -e "${RED}[INFO] Docker installing kubectl${NC}"
+sudo docker ps && echo -e "${GREEN}[INFO] Docker installed successful${NC}" || echo -e "${RED}[INFO] Error installing Docker${NC}"
+sudo rm -rf get-docker.sh
 
 # kubectl
 echo -e "${GREEN}---- Installing Kubectl ----${NC}"
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl && echo -e "${GREEN}[INFO] kubectl installed successful${NC}" || echo -e "${RED}[INFO] error installing kubectl${NC}"
-echo 'alias k=kubectl' >>~/.bashrc
+sudo rm -rf kubectl
+echo 'alias k=kubectl' >> ~/.bashrc
+echo 'alias k="sudo kubectl"' >> /home/afaraji/.bashrc
 
 # K3d
 echo -e "${GREEN}---- Installing K3d ----${NC}"
@@ -35,9 +38,43 @@ k3d version | grep version && echo -e "${GREEN}[INFO] K3d installed successful${
 
 # cluster configuration
 echo -e "${GREEN}=====================================================${NC}"
-echo -e "${GREEN}part 1 in the installation is done\n    start configuring the cluster ? [y/n] ${NC}"
+echo -e "part 1 in the installation is done\n    start configuring the cluster ? [y/n]"
 read ANSWER
-if [ ANSWER != 'y' ]
+if [ $ANSWER != 'y' ]
 then
 	exit
 fi
+############### argoCD install  #################
+
+sudo k3d cluster create dev-cluster --port 8080:80@loadbalancer --port 8888:8888@loadbalancer
+sudo kubectl create namespace argocd
+#sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+# ---> expose argoCD API Server by Changing the argocd-server service type to LoadBalancer
+#sudo kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+# ---> xpose argoCD by ingres conf
+#sudo wget https://raw.githubusercontent.com/argoproj/argo-cd/v2.0.1/manifests/install.yaml
+
+sudo kubectl apply -n argocd -f conf/install_argocd.yaml
+sudo kubectl apply -n argocd -f conf/ingress_argocd.yaml
+while true
+do
+	sudo kubectl get pods -n argocd | tail -n +2 | awk '{print $3}' | grep -v "Running"
+	if [ $? == 0 ]; then
+		break
+	else
+		echo "WAITING for argoCD..."
+	fi
+	sleep 1
+done
+###########check url: https://stackoverflow.com/questions/44140593/how-to-run-command-after-initialization ##########
+#lifecycle:
+#            postStart:
+#              exec:
+#                command: ["/bin/sh", "-c", {{cmd}}]
+##################3
+printf "${RED}###############################################\n"
+printf "connect on port 8080/argocd\n"
+printf "${GREEN}your credentiels are:${NC} admin:$(sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)\n"
+
+sudo kubectl apply -n argocd -f conf/app_argocd.yaml
+printf "${GREEN}################### DONE ######################${NC}\n"
